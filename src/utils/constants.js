5fa5a501faab6a51669ccf580300f0d678b945e3
Sourabh Kumar <bsourabhkumar@gmail.com>
2021-09-18 19:32:54 +0530

    some changes in constant file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,4 @@
-// import React from "react";
-import { GiCompass, GiDiamondHard, GiStabbedNote } from "react-icons/gi";
+
 export const links=[
     {
         id: 1,
@@ -14,29 +13,7 @@ export const links=[
     }
 ];
 
-export const services = [
-    {
-      id: 1,
-      icon: <GiCompass />,
-      title: "mission",
-      text:
-        "Committed to crafting products from the best quality materials to give you furniture that will last a lifetime"
-    },
-    {
-      id: 2,
-      icon: <GiDiamondHard />,
-      title: "vision",
-      text:
-        "To give our customers the best possible product not only best in terms of looks but also performance. We believe you are forming a long association with our furniture"
-    },
-    {
-      id: 3,
-      icon: <GiStabbedNote />,
-      title: "history",
-      text:
-        "Established in 2007 with the sole aim of creating best product with a small team and a great dream of providing a connection to our customers with our best design and material"
-    }
-  ];
+
 
   export const categories = [
         {
